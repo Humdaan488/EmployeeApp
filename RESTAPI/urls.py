97f5525611e243f892a869eb285277314456773c
Humdaan Javed <77771083+Humdaan488@users.noreply.github.com>
2023-10-12 13:05:00 -0400

    Add files via upload

---
+++ RESTAPI/urls.py
@@ -0,0 +1,28 @@
+#contains all links and functions to call for project
+"""
+URL configuration for DjangoRESTAPI project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+
+from django.conf.urls import url,include
+
+#list of URL patterns used by Django to determine how to handle incoming requests
+# URL patterns are mapped to different URL patterns
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    url(r'^',include('EmployeeApp.urls'))
+]
